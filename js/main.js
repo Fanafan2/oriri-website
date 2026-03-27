8c2a29228d95fa68b2a5760e4c8ece574d18797f
Gabriel Campos <g4brielcampos2109@gmail.com>
2026-03-27 13:33:05 -0600

    Landing page base terminada, puede mejorar, esto sera lo que se entregue para la etapa 2 del proyecto

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-const btn = document.getElementById('ej');
+const navLinks = document.querySelector('.nav-links');
+const hamburger = document.querySelectorAll('.hamburger');
+const openHam = document.getElementById('openHam');
 
-btn.addEventListener('click', () => console.log('hello world'))
+hamburger.forEach(ham => {
+    ham.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+        openHam.classList.toggle("itIsOpen");
+    });
+});
